b0ba4eb44467b3b6cc328456d2393c25c24bbb9e
lapinoub <ant.le.berre@gmail.com>
2017-07-29 17:16:28 +0200

    1er commit

---
+++ app/src/main/java/com/alb/cv_alb/MainActivity.java
@@ -0,0 +1,15 @@
+package com.alb.cv_alb;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
